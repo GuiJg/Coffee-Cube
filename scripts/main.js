2f49d5ff2eb227f9eedec635f4f9dcbf737f596b
GuiJg <j.gu1lh3rm325@gmail.com>
2023-12-27 00:21:20 -0300

    feat: menu page and slider menu

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,44 @@ window.addEventListener("scroll", function () {
         }
     }
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var splide = new Splide('.splide', {
+        perPage: 3,
+        rewind: true,
+    });
+
+    splide.mount();
+});
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+  slides.forEach((slide) => {
+    slide.style.display = 'none';
+  });
+
+  slides[index].style.display = 'block';
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+}
+
+// Show the first slide initially
+showSlide(currentSlide);
+
+// Add event listeners for next and previous buttons
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('.slider-container').innerHTML += `
+    <button onclick="prevSlide()">Previous</button>
+    <button onclick="nextSlide()">Next</button>
+  `;
+});
