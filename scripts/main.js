a202569c9043d74c67d0cd72e0b106af899cb730
GuiJg <j.gu1lh3rm325@gmail.com>
2023-12-28 14:51:55 -0300

    feat: responsive view mobile

--- scripts/main.js
+++ scripts/main.js
@@ -23,7 +23,25 @@ document.addEventListener('DOMContentLoaded', function () {
         speed: 1000,
     });
 
-    splide.mount();
+    checkWidth();
+    
+    // Adiciona um ouvinte de alteração de largura da janela
+    window.addEventListener('resize', checkWidth);
+
+    function checkWidth() {
+        // Verifica se a largura da janela é inferior a 1000 pixels
+        if (window.matchMedia('(max-width: 1000px)').matches) {
+            // Se for, ajusta o valor de perPage para 1
+            splide.options.perPage = 1;
+        } else {
+            // Se não for, volta para o valor original de perPage (3)
+            splide.options.perPage = 3;
+        }
+
+        // Atualiza o slider com as novas configurações
+        splide.destroy();
+        splide = new Splide('#splide1', splide.options).mount();
+    }
 });
 
 document.addEventListener('DOMContentLoaded', function () {
