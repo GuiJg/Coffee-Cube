3f446ca2ecc9abf914e769274a0f745677a3b998
GuiJg <j.gu1lh3rm325@gmail.com>
2023-12-28 16:08:06 -0300

    feat: animation text update

--- scripts/main.js
+++ scripts/main.js
@@ -66,9 +66,9 @@ function type() {
     if (charIndex < words[wordIndex].length) {
         textElement.innerHTML += words[wordIndex].charAt(charIndex);
         charIndex++;
-        setTimeout(type, 100);
+        setTimeout(type, 200);
     } else {
-        setTimeout(erase, 1500);
+        setTimeout(erase, 500);
     }
 }
 
@@ -76,7 +76,7 @@ function erase() {
     if (charIndex > 0) {
         textElement.innerHTML = words[wordIndex].substring(0, charIndex - 1);
         charIndex--;
-        setTimeout(erase, 50);
+        setTimeout(erase, 150);
     } else {
         wordIndex = (wordIndex + 1) % words.length;
         setTimeout(type, 500);
