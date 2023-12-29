5e51ccb1e8969b8b84d0b48f776591565f1e481e
GuiJg <j.gu1lh3rm325@gmail.com>
2023-12-28 21:49:48 -0300

    feat: slider feedback added

--- scripts/main.js
+++ scripts/main.js
@@ -101,3 +101,28 @@ function erase() {
 }
 
 type();
+
+const sliderContainer = document.getElementById('slider-container');
+const sliderContent = document.getElementById('slider-content');
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+
+let currentIndex = 0;
+
+function updateSlider() {
+    const newPosition = -currentIndex * 100 + '%';
+    sliderContent.style.transform = 'translateX(' + newPosition + ')';
+}
+
+function showPrevSlide() {
+    currentIndex = Math.max(0, currentIndex - 1);
+    updateSlider();
+}
+
+function showNextSlide() {
+    currentIndex = Math.min(1, currentIndex + 1);
+    updateSlider();
+}
+
+prevButton.addEventListener('click', showPrevSlide);
+nextButton.addEventListener('click', showNextSlide);
